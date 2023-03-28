6091b742c999b80509fa571d845409a13826cb2d
Ashish Singh <paradoxclover66@gmail.com>
2023-03-28 14:33:33 +0530

    Navbar

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,27 +2,22 @@ import React from "react";
 
 function Nav() {
   return (
-    <div className="flex flex-col sm:flex-row justify-between">
-      <div className="basis-1/4 text-lg">
-        <a href="/">Ashish Singh</a>
+    <div className="flex flex-row sm:justify-end justify-start">
+      <div className="mr-5">
+        <a href="/">About</a>
       </div>
-      <div className="flex flex-row justify-around">
-        <div className="mr-5">
-          <a href="/">About</a>
-        </div>
-        <div className="mr-5">
-          <a href="#experience">Experience</a>
-        </div>
-        <div className="mr-5">
-          <a href="#projects">Projects</a>
-        </div>
-        {/* TODO:
+      <div className="mr-5">
+        <a href="#experience">Experience</a>
+      </div>
+      <div className="mr-5">
+        <a href="#projects">Projects</a>
+      </div>
+      {/* TODO:
          <div className="mr-5">
           <a href="#blog">Blog</a>
         </div> */}
-        <div>
-          <a href="#contact">Contact</a>
-        </div>
+      <div>
+        <a href="#contact">Contact</a>
       </div>
     </div>
   );
